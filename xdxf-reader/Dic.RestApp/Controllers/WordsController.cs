d5aaca9f7a47f4a23ff5f5c4c4206a5673afde4b
tmteam <tmteam@yandex.ru>
2020-01-26 22:28:50 +0300

    Pirmitive words api

--- xdxf-reader/Dic.RestApp/Controllers/WordsController.cs
+++ xdxf-reader/Dic.RestApp/Controllers/WordsController.cs
@@ -2,52 +2,73 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Web;
+using Dic.Logic.DAL;
+using Dic.Logic.Dictionaries;
+using Dic.Logic.Services;
+using Dic.Logic.yapi;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Dic.RestApp.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("/")]
     public class WordsController : ControllerBase
     {
-        [HttpGet("Health")]
-        public HealthResponse GetHealth()
+        private YandexApiClient _yandexApiClient;
+        private NewWordsService _wordsService;
+
+        public WordsController()
         {
-            return new HealthResponse(HealthStatus.Online);
+            _yandexApiClient = new YandexApiClient("dict.1.1.20200117T131333Z.11b4410034057f30.cd96b9ccbc87c4d9036dae64ba539fc4644ab33d",
+                TimeSpan.FromSeconds(5));
+            _wordsService = new NewWordsService(new RuengDictionary(), new WordsRepository());
+        }
+        [HttpGet("Health")]
+        public async Task<HealthResponse> GetHealth()
+        {
+            var pingResult =  await _yandexApiClient.Ping();
+            if (pingResult)
+                return new HealthResponse(HealthStatus.Online);
+            else
+                return new HealthResponse(HealthStatus.Offline);
         }
 
         [HttpGet("Translation/{word}")]
-        public TranslationResponse GetTranslation(string word)
+        public async Task<TranslationResponse> GetTranslation(string word)
         {
-            return new TranslationResponse(
-                word,
-                new Translation("Мими", TranslationSource.Yadic),
-                new Translation("Бубубу", TranslationSource.Yadic)
-                );
+            var origin = HttpUtility.UrlDecode(word);
+
+            var yandexResponse = await _yandexApiClient.Translate(origin);
+            var result = yandexResponse.SelectMany(t => t.Tr)
+                .Select(t => new Translation(t.Text, TranslationSource.Yadic)).ToArray();
+            return new TranslationResponse(origin, result);
         }
+
         [HttpPut("Words")]
-        public void AddTranslation([FromBody] AddTranslationRequest translationRequest)
+        public void SelectTranslation([FromBody] AddTranslationRequest translationRequest)
         {
-            Console.WriteLine("Add translation for " + translationRequest.Word);
-            /*
-             * request-body: {
-                  Word: "War",
-                  Translations:[
-                    {
-                      Text: "Война"
-                      Source: "yadic|yatrans|local|manual"
-                    },
-                    {
-                      Text: "Культяпка"
-                      Source: "yadic|yatrans|local|manual"
-                    }
-                  ]  
-                }
-             */
+            var translations = translationRequest.Translations.Select(t => t.Text);
+            var translationsString = string.Join(", ", translations);
+
+            _wordsService.SaveForExams(translationRequest.Word, translationsString, null);
         }
 
     }
+    public class HealthResponse
+    {
+        public HealthResponse(HealthStatus status)
+        {
+            Status = status.ToString();
+        }
 
+        public string Status { get; }
+    }
+    public enum HealthStatus
+    {
+        Online,
+        Offline
+    }
     public class AddTranslationRequest
     {
         public string Word { get; set; }
@@ -82,18 +103,5 @@ namespace Dic.RestApp.Controllers
         public Translation[] Translations { get; }
     }
 
-    public enum HealthStatus
-    {
-        Online,
-        Offline
-    }
-    public class HealthResponse
-    {
-        public HealthResponse(HealthStatus status)
-        {
-            Status = status.ToString();
-        }
-
-        public string Status { get; }
-    }
+  
 }
