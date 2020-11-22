b14a07c89818bfb3db4977355b0fa926caf78085
tmteam <tmteam@yandex.ru>
2020-11-22 22:09:35 +0300

    Implement ExampleRepo

---
+++ Chtotiskazal/SayWhat.MongoDAL/Examples/Example.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Bson;
+
+namespace SayWhat.MongoDAL.Examples
+{
+    public class Example
+    {
+        public ObjectId Id { get; set; }
+        public string OriginWord { get; set; }
+        public string TranslatedWord { get; set; }
+        public string OriginExample { get; set; }
+        public string TranslatedExample { get; set; }
+    }
+}
