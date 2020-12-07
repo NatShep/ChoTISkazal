f9a5769b2bfa81a66d954fccad61339d6c78b1f2
NataSh <N.Shepina@bk.ru>
2020-12-08 02:44:52 +0300

    Fixed EngChooseWordInPhraseExam(simplify LINQ)

--- Chtotiskazal/Chotiskazal.Bot/Questions/EngChooseWordInPhraseExam.cs
+++ Chtotiskazal/Chotiskazal.Bot/Questions/EngChooseWordInPhraseExam.cs
@@ -14,26 +14,19 @@ namespace Chotiskazal.Bot.Questions
 
         public string Name => "Eng Choose word in phrase";
 
-        public async Task<ExamResult> Pass(ChatIO chatIo, UsersWordsService service, UserWordModel word, UserWordModel[] examList)
+        public async Task<ExamResult> Pass(ChatIO chatIo, UsersWordsService service, UserWordModel word,
+            UserWordModel[] examList)
         {
             if (!word.Phrases.Any())
                 return ExamResult.Impossible;
-            
+
             var phrase = word.GetRandomExample();
-            
+
             var replaced = phrase.OriginPhrase.Replace(phrase.OriginWord, "...");
-           
+
             if (replaced == phrase.OriginPhrase)
                 return ExamResult.Impossible;
-            
-            var otherExamples = examList
-                .SelectMany(e => e.Phrases)
-                .Where(p => !string.Equals(p.TranslatedPhrase, phrase.TranslatedPhrase,StringComparison.InvariantCultureIgnoreCase))
-                .ToArray();
 
-            if(!otherExamples.Any())
-                return ExamResult.Impossible;
-            
             var sb = new StringBuilder();
             sb.AppendLine($"\"{phrase.TranslatedPhrase}\"");
             sb.AppendLine();
@@ -42,22 +35,22 @@ namespace Chotiskazal.Bot.Questions
             sb.AppendLine($"\"{replaced}\"");
             sb.AppendLine($"Choose missing word...");
 
-            var variants = otherExamples
-                .Select(e => e.OriginWord).Where(e=>e!=phrase.OriginWord)
-                .Distinct()
+            var variants = examList
+                .Where(e => !e.Phrases.Select(p => p.TranslatedPhrase).ToList().Contains(phrase.TranslatedPhrase))
+                .Select(w => w.Word)
                 .Randomize()
                 .Take(5)
                 .Append(phrase.OriginWord)
                 .Randomize()
                 .ToArray();
-            
-            var _ =chatIo.SendMessageAsync(sb.ToString(), InlineButtons.CreateVariants(variants));
+
+            var _ = chatIo.SendMessageAsync(sb.ToString(), InlineButtons.CreateVariants(variants));
 
             var choice = await chatIo.TryWaitInlineIntKeyboardInput();
             if (choice == null)
                 return ExamResult.Retry;
 
-            if (string.Equals(variants[choice.Value],word.Word, StringComparison.InvariantCultureIgnoreCase))
+            if (string.Equals(variants[choice.Value], word.Word, StringComparison.InvariantCultureIgnoreCase))
             {
                 await service.RegisterSuccess(word);
                 return ExamResult.Passed;
