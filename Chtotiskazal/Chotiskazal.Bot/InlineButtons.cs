e74142d4cbba638670891de22cb5bcc8a46c3a58
NataSh <N.Shepina@bk.ru>
2020-12-14 23:44:42 +0300

    Menu buttons now work as command

--- Chtotiskazal/Chotiskazal.Bot/InlineButtons.cs
+++ Chtotiskazal/Chotiskazal.Bot/InlineButtons.cs
@@ -8,13 +8,13 @@ namespace Chotiskazal.Bot
     public static class InlineButtons
     {
         public static readonly InlineKeyboardButton EnterWords = new InlineKeyboardButton
-            {CallbackData = "~EnterWords", Text = "Translate"};
+            {CallbackData = "/add", Text = "Translate"};
 
         public static readonly InlineKeyboardButton Exam = new InlineKeyboardButton
-            {CallbackData = "~Exam", Text = "Learn"};
+            {CallbackData = "/learn", Text = "Learn"};
 
         public static readonly InlineKeyboardButton Stats = new InlineKeyboardButton
-            {CallbackData = "~Stats", Text = "Stats"};
+            {CallbackData = "/stats", Text = "Stats"};
 
         public static InlineKeyboardButton[] CreateVariants(IEnumerable<string> variants)
             => variants.Select((v, i) => new InlineKeyboardButton
@@ -23,6 +23,13 @@ namespace Chotiskazal.Bot
                 Text = v?? throw new InvalidDataException("Keyboard text cannot be null")
             }).ToArray();
         
+        public static InlineKeyboardButton[] CreateGhostVariants(IEnumerable<string> variants)
+            => variants.Select((v) => new InlineKeyboardButton
+            {
+                CallbackData = "-",
+                Text = v?? throw new InvalidDataException("Keyboard text cannot be null")
+            }).ToArray();
+
         public static InlineKeyboardButton[] CreateVariantsWithCancel(IEnumerable<string> variants)
         {
             return variants.Select((v, i) => new InlineKeyboardButton
