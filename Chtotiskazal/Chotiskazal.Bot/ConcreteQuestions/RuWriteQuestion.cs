1df6db468ebda961bcb606e8b0ef4a0227449bf8
tmteam <tmteam@yandex.ru>
2021-11-20 00:59:24 +0300

    Cleanup

--- Chtotiskazal/Chotiskazal.Bot/ConcreteQuestions/RuWriteQuestion.cs
+++ Chtotiskazal/Chotiskazal.Bot/ConcreteQuestions/RuWriteQuestion.cs
@@ -3,79 +3,113 @@ using System.Threading.Tasks;
 using Chotiskazal.Bot.Questions;
 using SayWhat.Bll;
 using SayWhat.Bll.Services;
+using SayWhat.MongoDAL;
 using SayWhat.MongoDAL.Words;
 
-namespace Chotiskazal.Bot.ConcreteQuestions
-{
-    public class RuWriteQuestion : IQuestion
-    {
-        private readonly LocalDictionaryService _localDictionaryService;
+namespace Chotiskazal.Bot.ConcreteQuestions {
 
-        public RuWriteQuestion(LocalDictionaryService localDictionaryService)
-        {
-            _localDictionaryService = localDictionaryService;
-        }
-        public bool NeedClearScreen => false;
-        public string Name => "Ru Write";
+public class RuWriteQuestion : IQuestion {
+    private readonly LocalDictionaryService _localDictionaryService;
 
-        public async Task<QuestionResult> Pass(ChatRoom chat, UserWordModel word,
-            UserWordModel[] examList)
-        {
-            var wordsInPhraseCount = word.Word.Count(c => c == ' ');
-            if (wordsInPhraseCount > 0 
-                && word.AbsoluteScore < wordsInPhraseCount * WordLeaningGlobalSettings.FamiliarWordMinScore)
-                return QuestionResult.Impossible;
-
-            await chat.SendMarkdownMessageAsync($"\\=\\=\\=\\=\\=\\>   *{word.AllTranslationsAsSingleString}*    \\<\\=\\=\\=\\=\\=\r\n" +
-                                          ""+chat.Texts.WriteTheTranslationMarkdown+"");
-            var enUserEntry = await chat.WaitUserTextInputAsync();
-
-            if (string.IsNullOrEmpty(enUserEntry))
-                return QuestionResult.RetryThisQuestion;
-
-            var comparation = word.Word.CheckCloseness(enUserEntry);
-            
-            if (comparation == StringsCompareResult.Equal)
-                return QuestionResult.Passed(chat.Texts);
-
-            if (comparation == StringsCompareResult.SmallMistakes)
-            {
-                await chat.SendMarkdownMessageAsync(chat.Texts.YouHaveATypoLetsTryAgainMarkdown(word.Word));
-                return QuestionResult.RetryThisQuestion;
-            }
-            
-            if (comparation == StringsCompareResult.BigMistakes)
-                return QuestionResult.Failed(chat.Texts.FailedMistakenMarkdown(word.Word), chat.Texts.Mistaken);
-            
-            
-            // ## Other translation case ##
-            
-            // example: 
-            //     Coefficient - коэффициент
-            //     Rate        - коэффициент
-            // Question is about 'коэффициент' (Coefficient)
-            // User answers 'Rate'
-            // Search for 'Rate' translations
-            var otherRuTranslationsOfUserInput = await _localDictionaryService.GetAllTranslationWords(enUserEntry.ToLower());
-            // if otherRuTranslationsOfUserInput contains 'коэффициент' or something like it
-            // then retry question
-            if (word.TextTranslations.Any(t1 =>
-                otherRuTranslationsOfUserInput.Any(t1.AreEqualIgnoreSmallMistakes)))
-            {
-                //translation is correct, but for other word
-                await chat.SendMessageAsync(
-                    $"{chat.Texts.CorrectTranslationButQuestionWasAbout} \"{word.Word}\" - *{word.AllTranslationsAsSingleString}*'\r\n" +
-                       chat.Texts.LetsTryAgain);
-                return QuestionResult.RetryThisQuestion;
-            }
-            
-            var translates = string.Join(",",otherRuTranslationsOfUserInput);
-            string failedMessage = "";
-            if(!string.IsNullOrWhiteSpace(translates))
-                failedMessage =$"{enUserEntry} {chat.Texts.translatesAs} {translates}\r\n";
-            failedMessage += $"{chat.Texts.RightTranslationWas}: *\"{word.Word}\"*";
-            return QuestionResult.Failed(failedMessage, 
-                chat.Texts);
-        }
+    public RuWriteQuestion(LocalDictionaryService localDictionaryService) {
+        _localDictionaryService = localDictionaryService;
     }
+
+    public bool NeedClearScreen => false;
+    public string Name => "Ru Write";
+
+    public Task<QuestionResult> Pass(ChatRoom chat, UserWordModel word, UserWordModel[] examList) =>
+        RuWriteQuestionHelper.PassRuWriteQuestion(
+            chat,
+            word,
+            word.AllTranslationsAsSingleString,
+            _localDictionaryService);
+}
+
+public class RuWriteSingleTarnslationQuestion : IQuestion {
+    private readonly LocalDictionaryService _localDictionaryService;
+
+    public RuWriteSingleTarnslationQuestion(LocalDictionaryService localDictionaryService) {
+        _localDictionaryService = localDictionaryService;
+    }
+
+    public bool NeedClearScreen => false;
+    public string Name => "Ru Write Single Translation";
+
+    public Task<QuestionResult> Pass(
+        ChatRoom chat, UserWordModel word,
+        UserWordModel[] examList) =>
+        RuWriteQuestionHelper.PassRuWriteQuestion(
+            chat, word, word.Translations.GetRandomItem().Word, _localDictionaryService);
+}
+
+static class RuWriteQuestionHelper {
+    public static async Task<QuestionResult> PassRuWriteQuestion(
+        ChatRoom chat,
+        UserWordModel word,
+        string translationCaption,
+        LocalDictionaryService localDictionaryService) {
+        var wordsInPhraseCount = word.Word.Count(c => c == ' ');
+        if (wordsInPhraseCount > 0 &&
+            word.AbsoluteScore < wordsInPhraseCount * WordLeaningGlobalSettings.FamiliarWordMinScore)
+            return QuestionResult.Impossible;
+
+        await chat.SendMarkdownMessageAsync(
+            $"\\=\\=\\=\\=\\=\\>   *{translationCaption}*    \\<\\=\\=\\=\\=\\=\r\n" +
+            chat.Texts.WriteTheTranslationMarkdown);
+
+        var enUserEntry = await chat.WaitUserTextInputAsync();
+
+        if (string.IsNullOrEmpty(enUserEntry))
+            return QuestionResult.RetryThisQuestion;
+
+        var comparation = word.Word.CheckCloseness(enUserEntry);
+
+        if (comparation == StringsCompareResult.Equal)
+            return QuestionResult.Passed(chat.Texts);
+
+        if (comparation == StringsCompareResult.SmallMistakes)
+        {
+            await chat.SendMarkdownMessageAsync(chat.Texts.YouHaveATypoLetsTryAgainMarkdown(word.Word));
+            return QuestionResult.RetryThisQuestion;
+        }
+
+        if (comparation == StringsCompareResult.BigMistakes)
+            return QuestionResult.Failed(chat.Texts.FailedMistakenMarkdown(word.Word), chat.Texts.Mistaken);
+        
+        // ## Other translation case ##
+
+        // example: 
+        //     Coefficient - коэффициент
+        //     Rate        - коэффициент
+        // Question is about 'коэффициент' (Coefficient)
+        // User answers 'Rate'
+        // Search for 'Rate' translations
+        var otherRuTranslationsOfUserInput = await localDictionaryService.GetAllTranslationWords(enUserEntry.ToLower());
+        
+        // if otherRuTranslationsOfUserInput contains 'коэффициент' or something like it
+        // then retry question
+        var russianTranslations = word.TextTranslations;
+        if (russianTranslations.Any(
+                t1 =>
+                    otherRuTranslationsOfUserInput.Any(t1.AreEqualIgnoreSmallMistakes)))
+        {
+            //translation is correct, but for other word
+            await chat.SendMessageAsync(
+                $"{chat.Texts.CorrectTranslationButQuestionWasAbout} \"{word.Word}\" - *{word.AllTranslationsAsSingleString}*'\r\n" +
+                chat.Texts.LetsTryAgain);
+            return QuestionResult.RetryThisQuestion;
+        }
+
+        var translates = string.Join(",", otherRuTranslationsOfUserInput);
+        string failedMessage = "";
+        if (!string.IsNullOrWhiteSpace(translates))
+            failedMessage = $"{enUserEntry} {chat.Texts.translatesAs} {translates}\r\n";
+        failedMessage += $"{chat.Texts.RightTranslationWas}: *\"{word.Word}\"*";
+        return QuestionResult.Failed(
+            failedMessage,
+            chat.Texts);
+    }
+}
+
 }
---
+++ Chtotiskazal/SayWhat.Tests/LocalDictionaryServiceTest.cs
@@ -0,0 +1,88 @@
+﻿using System.Threading.Tasks;
+using MongoDB.Bson;
+using NUnit.Framework;
+using SayWhat.Bll.Services;
+using SayWhat.MongoDAL.Dictionary;
+using SayWhat.MongoDAL.Examples;
+
+namespace SayWhat.MongoDAL.Tests {
+
+public class LocalDictionaryServiceTest {
+    private LocalDictionaryRepo _dictionaryRepo;
+    private ExamplesRepo _examplesRepo;
+    private LocalDictionaryService _service;
+
+    [SetUp]
+    public void Init() {
+        MongoTestHelper.DropAllCollections();
+        _dictionaryRepo = new LocalDictionaryRepo(MongoTestHelper.Database);
+        _examplesRepo = new ExamplesRepo(MongoTestHelper.Database);
+        _service = new LocalDictionaryService(_dictionaryRepo, _examplesRepo);
+    }
+
+    [Test]
+    public async Task AddWordToDictionary_GetAllTranslationWords() {
+        await _service.AddNewWord(CreateWord("rate", "", ("коэффициент", new Example[0])));
+        await _service.AddNewWord(CreateWord("coefficient", "", ("коэффициент", new Example[0])));
+
+        var rus = await _service.GetAllTranslationWords("rate".ToLower());
+        Assert.AreEqual(1, rus.Length);
+        Assert.AreEqual("коэффициент", rus[0]);
+    }
+
+    /**
+     * // ## Other translation case ##
+            
+            // example: 
+            //     Coefficient - коэффициент
+            //     Rate        - коэффициент
+            // Question is about 'коэффициент' (Coefficient)
+            // User answers 'Rate'
+            // Search for 'Rate' translations
+            var otherRuTranslationsOfUserInput = await _localDictionaryService.GetAllTranslationWords(enUserEntry.ToLower());
+     */
+    [Test]
+    public async Task AddWordToDictionary_GetWithExampleReturnsSame() {
+        var example = new Example {
+            Direction = TranslationDirection.EnRu,
+            Id = ObjectId.GenerateNewId(),
+            OriginWord = "table",
+            TranslatedWord = "стол",
+            OriginPhrase = "What the table?",
+            TranslatedPhrase = "Какого стола?"
+        };
+        
+        var word = CreateWord("table", "qweqwe", ("Стол", new[] { example }));
+        await _service.AddNewWord(word);
+        var translations = await _service.GetTranslationsWithExamples("table");
+        Assert.AreEqual(1, translations.Count);
+        var translation = translations[0];
+        Assert.AreEqual(word.Source, translation.Source);
+        Assert.AreEqual(word.Transcription, translation.EnTranscription);
+        Assert.AreEqual(word.Word, translation.OriginText);
+        Assert.AreEqual(word.Translations[0].Word, translation.TranslatedText);
+
+        Assert.AreEqual(example.OriginWord, translation.Examples[0].OriginWord);
+        Assert.AreEqual(example.OriginPhrase, translation.Examples[0].OriginPhrase);
+        Assert.AreEqual(example.TranslatedWord, translation.Examples[0].TranslatedWord);
+        Assert.AreEqual(example.TranslatedPhrase, translation.Examples[0].TranslatedPhrase);
+    }
+
+    public static DictionaryWord CreateWord(string en, string transcription, params (string, Example[])[] translations) {
+        return  new DictionaryWord {
+            Id = ObjectId.GenerateNewId(),
+            Word = en,
+            Language = Language.En,
+            Source = TranslationSource.Yadic,
+            Transcription = transcription,
+            Translations = translations.SelectToArray(t=> new DictionaryTranslation {
+                Word = t.Item1,
+                Language = Language.Ru,
+                Examples = t.Item2.SelectToArray(i=>new DictionaryReferenceToExample(i))
+            })
+        };
+    }
+}
+
+
+}
